1f15ad7c3a81a98b649ebe32cd601dc223486a51
Syed Refaie <syedmrefaie@gmail.com>
2022-06-13 15:25:29 -0700

    Generate Computer choice for Rock-Paper-Scissors

--- scripts.js
+++ scripts.js
@@ -1,5 +1,25 @@
 
 
-function compterPlay() {
-    let computerChoice = Math.floor(Math.random()*(3-1+1))+1;
+function computerPlay() {
+
+    let computerRandom = Math.floor(Math.random()*(3-1+1))+1;
+    console.log(computerRandom);
+    computerRandom = computerRandom.toString();
+    console.log(typeof computerRandom);
+    let computerChoice;
+    
+
+    switch (computerRandom) {
+        case '1':
+            computerChoice = "Rock";
+            break;
+        case '2':
+            computerChoice = "Paper";
+            break;
+        case '3':
+            computerChoice = "Scissors";
+            break;
+    }
+
+    console.log(computerChoice);
 }
